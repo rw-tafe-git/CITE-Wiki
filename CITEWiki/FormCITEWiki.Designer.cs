e20f1b6f926ec1b57387eff789127c63655fdd0b
rw-tafe-git <124329775+rw-tafe-git@users.noreply.github.com>
2023-03-17 11:26:45 +0800

    Updated Progress

--- CITEWiki/FormCITEWiki.Designer.cs
+++ CITEWiki/FormCITEWiki.Designer.cs
@@ -28,204 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            ButtonAdd = new Button();
-            NameLabel = new ColumnHeader();
-            CategoryLabel = new ColumnHeader();
-            MyListViewItem = new ListView();
-            ButtonEdit = new Button();
-            ButtonDelete = new Button();
-            button2 = new Button();
-            TextBoxName = new TextBox();
-            TextBoxCategory = new TextBox();
-            TextBoxDefinition = new TextBox();
-            textBox4 = new TextBox();
-            ButtonOpen = new Button();
-            button4 = new Button();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            TextBoxStructure = new TextBox();
-            label5 = new Label();
-            SuspendLayout();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.NameLabel = new System.Windows.Forms.ColumnHeader();
+            this.CategoryLabel = new System.Windows.Forms.ColumnHeader();
+            this.ListViewProperties = new System.Windows.Forms.ListView();
+            this.ButtonEdit = new System.Windows.Forms.Button();
+            this.ButtonDelete = new System.Windows.Forms.Button();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.TextBoxName = new System.Windows.Forms.TextBox();
+            this.TextBoxCategory = new System.Windows.Forms.TextBox();
+            this.TextBoxDefinition = new System.Windows.Forms.TextBox();
+            this.TextBoxSearch = new System.Windows.Forms.TextBox();
+            this.ButtonOpen = new System.Windows.Forms.Button();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextBoxStructure = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
             // 
             // ButtonAdd
             // 
-            ButtonAdd.Location = new Point(12, 12);
-            ButtonAdd.Name = "ButtonAdd";
-            ButtonAdd.Size = new Size(75, 23);
-            ButtonAdd.TabIndex = 0;
-            ButtonAdd.Text = "Add";
-            ButtonAdd.UseVisualStyleBackColor = true;
-            ButtonAdd.MouseClick += ButtonAdd_Click;
+            this.ButtonAdd.Location = new System.Drawing.Point(12, 12);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAdd.TabIndex = 0;
+            this.ButtonAdd.Text = "Add";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonAdd_Click);
             // 
             // NameLabel
             // 
-            NameLabel.Text = "Name";
-            NameLabel.Width = 100;
+            this.NameLabel.Text = "Name";
+            this.NameLabel.Width = 100;
             // 
             // CategoryLabel
             // 
-            CategoryLabel.Text = "Category";
-            CategoryLabel.Width = 100;
+            this.CategoryLabel.Text = "Category";
+            this.CategoryLabel.Width = 100;
             // 
-            // MyListViewItem
+            // ListViewProperties
             // 
-            MyListViewItem.Columns.AddRange(new ColumnHeader[] { NameLabel, CategoryLabel });
-            MyListViewItem.Location = new Point(277, 58);
-            MyListViewItem.Name = "MyListViewItem";
-            MyListViewItem.Size = new Size(245, 319);
-            MyListViewItem.TabIndex = 1;
-            MyListViewItem.UseCompatibleStateImageBehavior = false;
-            MyListViewItem.View = View.Details;
-            MyListViewItem.SelectedIndexChanged += ListViewDisplay_SelectedIndexChanged;
+            this.ListViewProperties.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameLabel,
+            this.CategoryLabel});
+            this.ListViewProperties.Location = new System.Drawing.Point(277, 58);
+            this.ListViewProperties.Name = "ListViewProperties";
+            this.ListViewProperties.Size = new System.Drawing.Size(245, 319);
+            this.ListViewProperties.TabIndex = 1;
+            this.ListViewProperties.UseCompatibleStateImageBehavior = false;
+            this.ListViewProperties.View = System.Windows.Forms.View.Details;
+            this.ListViewProperties.SelectedIndexChanged += new System.EventHandler(this.ListViewProperties_SelectedIndexChanged);
+            this.ListViewProperties.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListViewProperties_MouseClick);
             // 
             // ButtonEdit
             // 
-            ButtonEdit.Location = new Point(93, 12);
-            ButtonEdit.Name = "ButtonEdit";
-            ButtonEdit.Size = new Size(75, 23);
-            ButtonEdit.TabIndex = 2;
-            ButtonEdit.Text = "Edit";
-            ButtonEdit.UseVisualStyleBackColor = true;
-            ButtonEdit.MouseClick += ButtonEdit_Click;
+            this.ButtonEdit.Enabled = false;
+            this.ButtonEdit.Location = new System.Drawing.Point(93, 12);
+            this.ButtonEdit.Name = "ButtonEdit";
+            this.ButtonEdit.Size = new System.Drawing.Size(75, 23);
+            this.ButtonEdit.TabIndex = 2;
+            this.ButtonEdit.Text = "Edit";
+            this.ButtonEdit.UseVisualStyleBackColor = true;
+            this.ButtonEdit.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonEdit_Click);
             // 
             // ButtonDelete
             // 
-            ButtonDelete.Location = new Point(174, 12);
-            ButtonDelete.Name = "ButtonDelete";
-            ButtonDelete.Size = new Size(75, 23);
-            ButtonDelete.TabIndex = 3;
-            ButtonDelete.Text = "Delete";
-            ButtonDelete.UseVisualStyleBackColor = true;
-            ButtonDelete.MouseClick += ButtonDelete_Click;
+            this.ButtonDelete.Enabled = false;
+            this.ButtonDelete.Location = new System.Drawing.Point(174, 12);
+            this.ButtonDelete.Name = "ButtonDelete";
+            this.ButtonDelete.Size = new System.Drawing.Size(75, 23);
+            this.ButtonDelete.TabIndex = 3;
+            this.ButtonDelete.Text = "Delete";
+            this.ButtonDelete.UseVisualStyleBackColor = true;
+            this.ButtonDelete.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonDelete_Click);
             // 
-            // button2
+            // ButtonSearch
             // 
-            button2.Location = new Point(447, 10);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 4;
-            button2.Text = "Search";
-            button2.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Location = new System.Drawing.Point(447, 11);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSearch.TabIndex = 4;
+            this.ButtonSearch.Text = "Search";
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            this.ButtonSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonSearch_Click);
             // 
             // TextBoxName
             // 
-            TextBoxName.Location = new Point(94, 67);
-            TextBoxName.Name = "TextBoxName";
-            TextBoxName.Size = new Size(155, 23);
-            TextBoxName.TabIndex = 5;
+            this.TextBoxName.Location = new System.Drawing.Point(94, 67);
+            this.TextBoxName.Name = "TextBoxName";
+            this.TextBoxName.Size = new System.Drawing.Size(155, 23);
+            this.TextBoxName.TabIndex = 5;
             // 
             // TextBoxCategory
             // 
-            TextBoxCategory.Location = new Point(94, 108);
-            TextBoxCategory.Name = "TextBoxCategory";
-            TextBoxCategory.Size = new Size(155, 23);
-            TextBoxCategory.TabIndex = 6;
+            this.TextBoxCategory.Location = new System.Drawing.Point(94, 108);
+            this.TextBoxCategory.Name = "TextBoxCategory";
+            this.TextBoxCategory.Size = new System.Drawing.Size(155, 23);
+            this.TextBoxCategory.TabIndex = 6;
             // 
             // TextBoxDefinition
             // 
-            TextBoxDefinition.Location = new Point(33, 206);
-            TextBoxDefinition.Multiline = true;
-            TextBoxDefinition.Name = "TextBoxDefinition";
-            TextBoxDefinition.Size = new Size(216, 171);
-            TextBoxDefinition.TabIndex = 7;
+            this.TextBoxDefinition.Location = new System.Drawing.Point(33, 206);
+            this.TextBoxDefinition.Multiline = true;
+            this.TextBoxDefinition.Name = "TextBoxDefinition";
+            this.TextBoxDefinition.Size = new System.Drawing.Size(216, 171);
+            this.TextBoxDefinition.TabIndex = 7;
             // 
-            // textBox4
+            // TextBoxSearch
             // 
-            textBox4.Location = new Point(277, 11);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(164, 23);
-            textBox4.TabIndex = 8;
+            this.TextBoxSearch.Location = new System.Drawing.Point(277, 11);
+            this.TextBoxSearch.Name = "TextBoxSearch";
+            this.TextBoxSearch.Size = new System.Drawing.Size(164, 23);
+            this.TextBoxSearch.TabIndex = 8;
             // 
             // ButtonOpen
             // 
-            ButtonOpen.Location = new Point(33, 415);
-            ButtonOpen.Name = "ButtonOpen";
-            ButtonOpen.Size = new Size(75, 23);
-            ButtonOpen.TabIndex = 9;
-            ButtonOpen.Text = "Open";
-            ButtonOpen.UseVisualStyleBackColor = true;
-            ButtonOpen.MouseClick += ButtonOpen_MouseClick;
+            this.ButtonOpen.Location = new System.Drawing.Point(33, 415);
+            this.ButtonOpen.Name = "ButtonOpen";
+            this.ButtonOpen.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOpen.TabIndex = 9;
+            this.ButtonOpen.Text = "Open";
+            this.ButtonOpen.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // ButtonSave
             // 
-            button4.Location = new Point(174, 415);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 23);
-            button4.TabIndex = 10;
-            button4.Text = "Save";
-            button4.UseVisualStyleBackColor = true;
+            this.ButtonSave.Location = new System.Drawing.Point(174, 415);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSave.TabIndex = 10;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(48, 70);
-            label2.Name = "label2";
-            label2.Size = new Size(39, 15);
-            label2.TabIndex = 13;
-            label2.Text = "Name";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name";
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(33, 188);
-            label3.Name = "label3";
-            label3.Size = new Size(59, 15);
-            label3.TabIndex = 14;
-            label3.Text = "Definition";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Definition";
             // 
             // label4
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(33, 108);
-            label4.Name = "label4";
-            label4.Size = new Size(55, 15);
-            label4.TabIndex = 15;
-            label4.Text = "Category";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Category";
             // 
             // TextBoxStructure
             // 
-            TextBoxStructure.Location = new Point(94, 148);
-            TextBoxStructure.Name = "TextBoxStructure";
-            TextBoxStructure.Size = new Size(155, 23);
-            TextBoxStructure.TabIndex = 16;
+            this.TextBoxStructure.Location = new System.Drawing.Point(94, 148);
+            this.TextBoxStructure.Name = "TextBoxStructure";
+            this.TextBoxStructure.Size = new System.Drawing.Size(155, 23);
+            this.TextBoxStructure.TabIndex = 16;
             // 
             // label5
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(33, 148);
-            label5.Name = "label5";
-            label5.Size = new Size(55, 15);
-            label5.TabIndex = 17;
-            label5.Text = "Structure";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Structure";
             // 
             // FormCITEWiki
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(552, 450);
-            Controls.Add(label5);
-            Controls.Add(TextBoxStructure);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(button4);
-            Controls.Add(ButtonOpen);
-            Controls.Add(textBox4);
-            Controls.Add(TextBoxDefinition);
-            Controls.Add(TextBoxCategory);
-            Controls.Add(TextBoxName);
-            Controls.Add(button2);
-            Controls.Add(ButtonDelete);
-            Controls.Add(ButtonEdit);
-            Controls.Add(MyListViewItem);
-            Controls.Add(ButtonAdd);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
-            Name = "FormCITEWiki";
-            Text = "Wiki Lookup";
-            Load += DataTableForm_Load;
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TextBoxStructure);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.ButtonOpen);
+            this.Controls.Add(this.TextBoxSearch);
+            this.Controls.Add(this.TextBoxDefinition);
+            this.Controls.Add(this.TextBoxCategory);
+            this.Controls.Add(this.TextBoxName);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ButtonDelete);
+            this.Controls.Add(this.ButtonEdit);
+            this.Controls.Add(this.ListViewProperties);
+            this.Controls.Add(this.ButtonAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormCITEWiki";
+            this.Text = "Wiki Lookup";
+            this.Load += new System.EventHandler(this.FormCITEWiki_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
@@ -233,16 +239,16 @@
         private Button ButtonAdd;
         private ColumnHeader NameLabel;
         private ColumnHeader CategoryLabel;
-        private ListView MyListViewItem;
+        private ListView ListViewProperties;
         private Button ButtonEdit;
         private Button ButtonDelete;
-        private Button button2;
+        private Button ButtonSearch;
         private TextBox TextBoxName;
         private TextBox TextBoxCategory;
         private TextBox TextBoxDefinition;
-        private TextBox textBox4;
+        private TextBox TextBoxSearch;
         private Button ButtonOpen;
-        private Button button4;
+        private Button ButtonSave;
         private Label label2;
         private Label label3;
         private Label label4;
--- CITEWiki/FormCITEWiki.cs
+++ CITEWiki/FormCITEWiki.cs
@@ -1,4 +1,6 @@
+using Microsoft.VisualBasic.Logging;
 using System.Data.Common;
+using System.Diagnostics;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace CITEWiki
@@ -10,8 +12,10 @@ namespace CITEWiki
         {
             InitializeComponent();
             InitializeArray();
+            ButtonDelete.Enabled = false;
             ButtonEdit.Enabled = false;
         }
+
         #region HelperMethods
         private void InitializeArray()
         {
@@ -23,39 +27,13 @@ namespace CITEWiki
             }
         }
         #endregion
+
         //9.1 - Create a global 2D string array, use static variables for the dimensions(row = 12, column = 4),
         static int rows = 12;
         static int columns = 4;
+        int selectedRow = -1;
+        int ptr = 0;
         string[,] DataTable = new string[rows, columns];
-
-
-        private void DisplayTable()
-        {
-            MyListViewItem.Items.Clear();
-            for (int x = 0; x < rows; x++)
-            {
-                ListViewItem lvi = new ListViewItem(DataTable[x, 0]);
-                lvi.SubItems.Add(DataTable[x, 1]);
-                lvi.SubItems.Add(DataTable[x, 2]);
-                lvi.SubItems.Add(DataTable[x, 3]);
-                lvi.SubItems.Add(DataTable[x, 4]);
-                MyListViewItem.Items.Add(lvi);
-            }
-        }
-
-        private void ButtonFill_Click(object sender, EventArgs e)
-        {
-            Random random = new Random();
-            for (int x = 0; x < rows; x++)
-            {
-                for (int y = 0; y < columns; y++)
-                {
-                    DataTable[x, y] = random.Next(10, 99).ToString();
-                }
-            }
-            DisplayTable();
-            ButtonEdit.Enabled = true;
-        }
 
         private void SortTable()
         {
@@ -65,7 +43,7 @@ namespace CITEWiki
                 {
                     if (string.Compare(DataTable[i, 0], DataTable[i + 1, 0]) == 1)
                     {
-                        Swap(i);
+                        SwapElements(x, i);
                     }
                 }
             }
@@ -78,36 +56,64 @@ namespace CITEWiki
             DisplayTable();
         }
 
-        private void ListViewDisplay_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void DataTableForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
         //9.2 - Create an ADD button that will store the information from the 4 text boxes into the 2D array,
         private void ButtonAdd_Click(object sender, MouseEventArgs e)
         {
-            ClearTextBox();
+            var newName = TextBoxName.Text;
+            var newCategory = TextBoxCategory.Text;
+            var newStructure = TextBoxStructure.Text;
+            var newDefinition = TextBoxDefinition.Text;
+
+            if (/*!string.IsNullOrEmpty(newName) && !string.IsNullOrEmpty(newCategory) && !string.IsNullOrEmpty(newStructure) && !string.IsNullOrEmpty(newDefinition) &&*/ ptr < rows)
+            {
+                DataTable[ptr, 0] = newName;
+                DataTable[ptr, 1] = newCategory;
+                DataTable[ptr, 2] = newStructure;
+                DataTable[ptr, 3] = newDefinition;
+
+                ptr++;
+                DisplayTable();
+                ButtonEdit.Enabled = false;
+                ButtonDelete.Enabled = false;
+
+                ClearTextBoxes();
+
+                Debug.WriteLine(ptr);
+            }
         }
 
         //9.3 - Create an EDIT button that will allow the user to modify any information from the 4 text boxes into the 2D array,
         private void ButtonEdit_Click(object sender, MouseEventArgs e)
         {
+            DataTable[selectedRow, 0] = TextBoxName.Text; ;
+            DataTable[selectedRow, 1] = TextBoxCategory.Text;
+            DataTable[selectedRow, 2] = TextBoxStructure.Text;
+            DataTable[selectedRow, 3] = TextBoxDefinition.Text;
 
+            ptr--;
+            DisplayTable();
+            ButtonEdit.Enabled = true;
+
+            ClearTextBoxes();
         }
 
         //9.4 - Create a DELETE button that removes all the information from a single entry of the array; the user must be prompted before the final deletion occurs,
         private void ButtonDelete_Click(object sender, MouseEventArgs e)
         {
+            DataTable[selectedRow, 0] = "~";
+            DataTable[selectedRow, 1] = "~";
+            DataTable[selectedRow, 2] = "~";
+            DataTable[selectedRow, 3] = "~";
 
+            ptr--;
+            DisplayTable();
+            ButtonEdit.Enabled = true;
+
+            ClearTextBoxes();
         }
 
         //9.5 - Create a CLEAR method to clear the four text boxes so a new definition can be added,
-        private void ClearTextBox()
+        private void ClearTextBoxes()
         {
             TextBoxName.Clear();
             TextBoxCategory.Clear();
@@ -118,26 +124,26 @@ namespace CITEWiki
         //9.6 - Write the code for a Bubble Sort method to sort the 2D array by Name ascending, ensure you use a separate swap method that passes the array element to be swapped (do not use any built-in array methods),
         private void BubbleSort()
         {
-            /*for (int x = 0; x < rows; x++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int i = 0; i < rows - 1; i++)
+                for (int j = 0; j < rows - 1; j++)
                 {
-                    if (string.Compare(DataTable[i, 0], DataTable[i + 1, 0]) == )
+                    if (string.CompareOrdinal(DataTable[i, 0], DataTable[j, 0]) < 0 )
                     {
-                        Swap(i);
+                        SwapElements(i, j);
                     }
                 }
-            }*/
+            }
         }
 
-        private void Swap(int indx)
+        private void SwapElements(int indxI, int indxJ)
         {
             string temp;
-            for (int z = 0; z < columns; z++)
+            for (int x = 0; x < columns; x++)
             {
-                temp = DataTable[indx, z];
-                DataTable[indx, z] = DataTable[indx + 1, z];
-                DataTable[indx + 1, z] = temp;
+                temp = DataTable[indxI, x];
+                DataTable[indxI, x] = DataTable[indxJ, x];
+                DataTable[indxJ, x] = temp;
             }
         }
 
@@ -147,14 +153,31 @@ namespace CITEWiki
 
         }
 
-        //9.8 - Create a display method that will show the following information in a ListView: Name and Category,
-        private void DisplayData()
+        private void ButtonSearch_Click(object sender, MouseEventArgs e)
         {
-            MyListViewItem.Items.Clear();
+            BubbleSort();
+            int startIndex = -1;
+            int finalIndex = ptr;
+            bool flag = false;
+            int foundIndex = -1;
+            /*while (!flag && !((finalIndex - startin)))
+            {
+                
+            }*/
+        }
+
+        //9.8 - Create a display method that will show the following information in a ListView: Name and Category,
+        private void DisplayTable()
+        {
+            BubbleSort();
+            ListViewProperties.Items.Clear();
             for (int x = 0; x < rows; x++)
             {
                 ListViewItem lvi = new ListViewItem(DataTable[x, 0]);
-                //lvi.SubItems.Add
+                lvi.SubItems.Add(DataTable[x, 1]);
+                lvi.SubItems.Add(DataTable[x, 2]);
+                lvi.SubItems.Add(DataTable[x, 3]);
+                ListViewProperties.Items.Add(lvi);
             }
         }
 
@@ -163,10 +186,33 @@ namespace CITEWiki
             BubbleSort();
             //Put binary search here
         }
-
+        
         private void SaveBinaryFile()
         {
 
+        }
+
+        private void FormCITEWiki_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListViewProperties_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListViewProperties_MouseClick(object sender, MouseEventArgs e)
+        {
+            selectedRow = ListViewProperties.SelectedIndices[0];
+
+            TextBoxName.Text = DataTable[selectedRow, 0];
+            TextBoxCategory.Text = DataTable[selectedRow, 1];
+            TextBoxStructure.Text = DataTable[selectedRow, 2];
+            TextBoxDefinition.Text = DataTable[selectedRow, 3];
+
+            ButtonEdit.Enabled = true;
+            ButtonDelete.Enabled = true;
         }
     }
 }
